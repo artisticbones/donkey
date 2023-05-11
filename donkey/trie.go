3f11bc2465c29bb6c826a4672fc7624b5b94d5c7
artisticbones <artisticbones@163.com>
2023-05-11 18:09:55 +0800

    trie

---
+++ donkey/trie.go
@@ -0,0 +1,42 @@
+package donkey
+
+type node struct {
+	pattern  string //待匹配路由，例如 /p/:lang
+	part     string // 路由中的一部分
+	children []*node
+	isWild   bool
+}
+
+func (n *node) matchChild(part string) *node {
+	for _, child := range n.children {
+		if child.part == part || child.isWild {
+			return child
+		}
+	}
+	return nil
+}
+
+func (n *node) matchChildren(part string) []*node {
+	nodes := make([]*node, 0, len(n.children))
+	for _, child := range n.children {
+		if child.part == part || child.isWild {
+			nodes = append(nodes, child)
+		}
+	}
+	return nodes
+}
+
+func (n *node) insert(pattern string, parts []string, height int) {
+	if len(parts) == height {
+		n.pattern = pattern
+		return
+	}
+
+	part := parts[height]
+	child := n.matchChild(part)
+	if child == nil {
+		child = &node{part: part, isWild: part[0] == ':' || part[0] == '*'}
+		n.children = append(n.children, child)
+	}
+	child.insert(pattern, parts, height+1)
+}
